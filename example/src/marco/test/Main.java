63df0857c85e7c5c9a46ff7dfec0e81b49f48214
marcosantos98 <marcomsantos98@gmail.com>
2023-09-14 00:50:08 +0100

    Check for arguments when we run the jar

--- example/src/marco/test/Main.java
+++ example/src/marco/test/Main.java
@@ -1,10 +1,13 @@
 package marco.test;
 
+import java.util.Arrays;
+
 import marco.test.math.Sum;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Given args: " + Arrays.toString(args));
         System.out.printf("34 + 35 = %d\n", Sum.add(34, 35));
     }
 }
