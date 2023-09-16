dcbabb1e2faa55aeff83b07eba228a33592c3590
marcosantos98 <marcomsantos98@gmail.com>
2023-09-16 08:55:16 +0100

    Use external lib Sum.add method

--- example/src/marco/test/Main.java
+++ example/src/marco/test/Main.java
@@ -2,11 +2,11 @@ package marco.test;
 
 import java.util.Arrays;
 
-import marco.test.math.Sum;
+import marco.math.Sum;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello, World!");
         System.out.println("Given args: " + Arrays.toString(args));
         System.out.printf("34 + 35 = %d\n", Sum.add(34, 35));
     }
