52de536218210ded738fba1765a2566cee766ce2
marcosantos98 <marcomsantos98@gmail.com>
2023-10-18 23:03:38 +0100

    Add example

--- example/src/main/marco/test/Main.java
+++ example/src/main/marco/test/Main.java
@@ -2,9 +2,12 @@ package marco.test;
 
 import java.util.Arrays;
 
+import org.apache.commons.math.util.MathUtils;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Given args: " + Arrays.toString(args));
+        System.out.println("CommonsMath TWO_PI: " + MathUtils.TWO_PI);
     }
 }
