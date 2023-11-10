a5538387b9e26ff18c3db0af6f1b9094ad24840b
marcosantos98 <marcomsantos98@gmail.com>
2023-11-10 15:57:34 +0000

    Add new example test

---
+++ example/src/tests/marco/tests/OtherTest.java
@@ -0,0 +1,13 @@
+package marco.tests;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class OtherTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
